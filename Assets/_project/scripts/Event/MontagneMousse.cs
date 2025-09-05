3d47ab02b2f8a0d3832bf2aa59a71ba32b03a0ed
Novaely <adam.guerfi06@gmail.com>
2025-09-05 13:18:35 +0200

    [ADD] sound mousse + UI menu

--- Assets/_project/scripts/Event/MontagneMousse.cs
+++ Assets/_project/scripts/Event/MontagneMousse.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace GFM2025
 {
     public class MontagneMousse : EventParent
     {
+        public UnityEvent OnMousse;
+
         private void OnTriggerEnter(Collider other) {
             if(other.gameObject.TryGetComponent<IPlayerBehaviour>(out IPlayerBehaviour player)) {
                 if (UI_QTE.Instance.StartQTE(gameObject))
                 {
+                    OnMousse?.Invoke();
                     _timePass = true;
                 }
             }
