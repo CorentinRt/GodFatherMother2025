7fec7bf19f681170e2a472c46700022d7e764c20
Novaely <adam.guerfi06@gmail.com>
2025-09-05 09:42:46 +0200

    [modif] dogtoy finish +  asset slime UI

--- Assets/_project/scripts/Event/DogToy.cs
+++ Assets/_project/scripts/Event/DogToy.cs
@@ -4,13 +4,34 @@ namespace GFM2025
 {
     public class DogToy : EventParent {
         private Rigidbody _rgbd;
+        [SerializeField] private LayerMask _mask;
+
+        [SerializeField] private DogToyDataBase _data;
+
+        private int NumberBounce = 1;
 
         private void OnEnable() {
             TryGetComponent(out _rgbd);
+            transform.position = new Vector3(transform.position.x, transform.position.y+5, transform.position.z);
+            _rgbd.maxLinearVelocity = _data.maxVelocity;
         }
 
         private void Start() {
-            _rgbd.linearVelocity = new Vector3(Random.Range(-0.5f, 0.5f), 0, Random.Range(-0.5f, 0.5f));
+            _rgbd.linearVelocity = new Vector3(Random.Range(-2f, 2f), 0, Random.Range(-2f, 2f));
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if ((( 1 << collision.gameObject.layer) & _mask)  != 0)
+            {
+                NumberBounce++;
+                _rgbd.AddForce(Vector3.Reflect(collision.transform.position - transform.position, collision.contacts[0].normal) * _data.bounce * NumberBounce* _data.scaleSpeed, ForceMode.Impulse);
+            }
+            if (collision.transform.tag == "Player")
+            {
+                _rgbd.AddForce(Vector3.Reflect(collision.transform.position - transform.position, collision.contacts[0].normal) * _data.bounce, ForceMode.Impulse);
+                PlayerBehaviour.Instance.BouncePlayerBack();
+            }
         }
     }
 }
